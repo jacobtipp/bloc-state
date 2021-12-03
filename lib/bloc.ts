577d8cf53c3d7b0536362d3f5d0b3b31c233b270
Jacob Tipp <jacobtipp@gmail.com>
2021-12-03 15:16:35 -0800

    fixed: cleaned up comments

--- lib/bloc.ts
+++ lib/bloc.ts
@@ -1,84 +1,65 @@
 import { EMPTY, Observable, Subject, throwError } from "rxjs";
 import { catchError, concatMap, mergeMap, tap } from "rxjs/operators";
 import { Cubit } from "./cubit";
-import { BlocState } from "./state";
+import { BlocTState } from "./state";
 import { asyncGeneratorToObservable } from "./util";
 
-export abstract class Bloc<Event, State> extends Cubit<State> {
-  constructor(state: State) {
+export abstract class Bloc<TEvent, TState> extends Cubit<TState> {
+  private readonly _events$ = new Subject<TEvent>();
+
+  private _event: TEvent | undefined;
+
+  constructor(state: TState) {
     super(state);
-    this._subscribeToEvents();
+    this._subscribeToTEvents();
   }
 
-  /**
-   * local
-   */
+  protected get state(): TState {
+    return this._state;
+  }
 
-  private readonly _events$ = new Subject<Event>();
-  private _event: Event | undefined;
-
-  /**
-   * public
-   */
-
-  addEvent(event: Event): void {
+  addTEvent(event: TEvent): void {
     if (!this._events$.closed) {
       this._events$.next(event);
     }
   }
 
-  close(): void {
-    this._dispose();
-  }
-
-  /**
-   * protected
-   */
-
-  protected override onTransition?(current: State, next: State, event?: Event): void;
+  protected override onTransition?(current: TState, next: TState, event?: TEvent): void;
 
   protected override onError?(error: Error): void;
 
-  protected onEvent?(event: Event): void;
+  protected onTEvent?(event: TEvent): void;
 
-  protected transitionHandler(current: State, next: State) {
+  protected transitionHandler(current: TState, next: TState) {
     if (this.onTransition) {
       this.onTransition(current, next, this._event);
     }
   }
 
-  protected abstract mapEventToState(event: Event): AsyncGenerator<State>;
+  protected abstract mapTEventToTState(event: TEvent): AsyncGenerator<TState>;
 
-  protected get state(): State {
-    return this._state;
-  }
-
-  /**
-   * private
-   */
-
-  private _subscribeToEvents(): void {
+  private _subscribeToTEvents(): void {
     this._events$
       .pipe(
         tap((event) => {
-          if (this.onEvent) {
-            this.onEvent(event);
+          if (this.onTEvent) {
+            this.onTEvent(event);
           }
         }),
-        concatMap((event) => this._mapEventToState(event)),
+        concatMap((event) => this._mapTEventToTState(event)),
         tap((state) => this.emit(state))
       )
       .subscribe();
   }
 
-  private _mapEventToState(event: Event): Observable<State> {
-    return asyncGeneratorToObservable(this.mapEventToState(event)).pipe(
-      catchError((error) => this._mapEventToStateError(error)),
+  private _mapTEventToTState(event: TEvent): Observable<TState> {
+    return asyncGeneratorToObservable(this.mapTEventToTState(event)).pipe(
+      catchError((error) => this._mapTEventToTStateError(error)),
       tap(() => (this._event = event))
     );
   }
 
-  private _mapEventToStateError(error: Error): Observable<never> {
+  private _mapTEventToTStateError(error: Error): Observable<never> {
     if (this.onError) {
       this.onError(error);
     }
@@ -88,5 +69,9 @@ export abstract class Bloc<Event, State> extends Cubit<State> {
   private _dispose(): void {
     this._events$.complete();
     super.dispose();
+  }
+
+  close(): void {
+    this._dispose();
   }
 }
--- lib/cubit.ts
+++ lib/cubit.ts
@@ -3,6 +3,12 @@ import { distinctUntilChanged, filter, map, shareReplay, skip, tap } from "rxjs/
 import { BlocState } from ".";
 
 export abstract class Cubit<T = any> {
+  private readonly _state$: Observable<T>;
+
+  private readonly _stateSubject$: BehaviorSubject<T>;
+
+  state$: Observable<T>;
+
   constructor(protected _state: T) {
     this._stateSubject$ = new BehaviorSubject(_state);
     this._state$ = this._buildStatePipeline();
@@ -10,38 +16,43 @@ export abstract class Cubit<T = any> {
     this._listen();
   }
 
-  /**
-   * @description
-   * * public state observable
-   */
-  state$: Observable<T>;
-
-  /**
-   * * Getter to retrieve the current snapshot of the state
-   *  @returns {T}
-   */
   protected get state(): T {
     return this._state;
   }
 
-  /**
-   * @description
-   * * private _state observable used to build a pipeline for stateSubject
-   */
-  private readonly _state$: Observable<T>;
+  protected onTransition?(current: T, next: T): void;
 
-  /**
-   * @description
-   * * the BehaviorSubject of a Cubit
-   */
-  private readonly _stateSubject$: BehaviorSubject<T>;
+  protected onError?(error: Error): void;
 
-  /**
-   * @description
-   * * State that is pushed to a Cubit passes through this pipeline
-   *
-   * @returns {Observable<T>}
-   */
+  protected transitionHandler(current: T, next: T) {
+    if (this.onTransition) {
+      this.onTransition(current, next);
+    }
+  }
+
+  protected errorHandler(error: Error) {
+    if (this.onError) {
+      this.onError(error);
+    }
+  }
+
+  protected select<K>(mapState: (state: T) => K): Observable<K> {
+    return this._state$.pipe(
+      map((state) => mapState(state)),
+      distinctUntilChanged()
+    );
+  }
+
+  protected emit(newState: T): void {
+    if (!this._stateSubject$.closed) {
+      this._stateSubject$.next(newState);
+    }
+  }
+
+  protected dispose(): void {
+    this._stateSubject$.complete();
+  }
+
   private _buildStatePipeline(): Observable<T> {
     return this._stateSubject$.asObservable().pipe(
       distinctUntilChanged(),
@@ -55,97 +66,12 @@ export abstract class Cubit<T = any> {
     );
   }
 
-  /**
-   * @description
-   * * subscribe to stateSubject when a Cubit is instantiated
-   */
   private _listen() {
     this._state$.subscribe({
       error: (error) => this.errorHandler(error),
     });
   }
 
-  protected onTransition?(current: T, next: T): void;
-  protected onError?(error: Error): void;
-
-  /**
-   *
-   * @param current: T
-   * @param next: T
-   *
-   * @override
-   * @description
-   * * before the next state is emitted, provide a transition handler function that a
-   * * derived class can implement
-   */
-  protected transitionHandler(current: T, next: T) {
-    if (this.onTransition) {
-      this.onTransition(current, next);
-    }
-  }
-
-  /**
-   *
-   * @param error
-   *
-   * @override
-   * @description
-   * * Uncaught Cubit errors will be emitted to an erroHandler of a derived class
-   * *
-   */
-  protected errorHandler(error: Error) {
-    if (this.onError) {
-      this.onError(error);
-    }
-  }
-
-  /**
-   * * Creates an Observable stream mapped to only a selected part of the state.
-   * * The stream will emit data only when the mapped portion has changed. An optional source
-   * * stream can be provided as a second parameter as long as it returns <T>.
-   * @param mapState
-   * @returns {Observable<K>} Observable<T>
-   */
-  protected select<K>(mapState: (state: T) => K): Observable<K> {
-    return this._state$.pipe(
-      map((state) => mapState(state)),
-      distinctUntilChanged()
-    );
-  }
-
-  /**
-   * * Filter any newly emitted state based on filter function criteria
-   * @param filterState
-   * @returns  {Observable<T>}
-   */
-  protected filter(filterState: (state: T) => boolean): Observable<T> {
-    return this._state$.pipe(
-      filter((state) => filterState(state)),
-      distinctUntilChanged()
-    );
-  }
-
-  /**
-   * * Push a new immutable state snapshot
-   * @param {T} newState
-   */
-  protected emit(newState: T): void {
-    if (!this._stateSubject$.closed) {
-      this._stateSubject$.next(newState);
-    }
-  }
-
-  /**
-   * * Dispose the cubit by setting the state to complete, closing the stream
-   */
-  protected dispose(): void {
-    this._stateSubject$.complete();
-  }
-
-  /**
-   * @description
-   * * close Bloc,
-   */
   close() {
     this.dispose();
   }
