b23e871f332dffa89fbaac95397ee5312080fc39
Jacob Tipp <jacobtipp@gmail.com>
2022-07-02 11:33:36 -0700

    updated: added comments to bloc and cubit

--- lib/bloc.ts
+++ lib/bloc.ts
@@ -14,8 +14,8 @@ export interface Type<T = any> extends Function {
 export abstract class Bloc<E extends BlocEvent, State> extends Cubit<State> {
   constructor(state: State) {
     super(state);
-    this._mapEventToState = this._mapEventToState.bind(this);
     this.emit = this.emit.bind(this);
+    this._mapEventToState = this._mapEventToState.bind(this);
     this._eventsSubscription = this._subscribeToEvents();
   }
 
@@ -25,6 +25,13 @@ export abstract class Bloc<E extends BlocEvent, State> extends Cubit<State> {
 
   private _event: E | undefined;
 
+  /**
+   *
+   * @param event
+   * @param eventHandler
+   * @descrition this method is for registering event handlers based on the type of events that
+   * are added to a bloc
+   */
   protected on<T extends E>(event: Type<T>, eventHandler: EventHandler<T, State>) {
     if (this._eventsMap.has(event.name)) {
       throw new Error(`Error: ${event} can only have one EventHandler`);
@@ -33,15 +40,25 @@ export abstract class Bloc<E extends BlocEvent, State> extends Cubit<State> {
     this._eventsMap.set(event.name, eventHandler);
   }
 
+  /**
+   * @returns the current state
+   */
   protected override get state(): State {
     return this._state;
   }
 
+  /**
+   *
+   * @param event Event: E
+   * @description add a new bloc event to the bloc event stream
+   */
   addEvent(event: E): void {
     if (!this._events$.closed) {
       this._events$.next(event);
     }
   }
+
+  // overridable methods for transtions, changes, and errors
 
   protected override listen(state: State) {
     return;
@@ -63,10 +80,20 @@ export abstract class Bloc<E extends BlocEvent, State> extends Cubit<State> {
     return;
   }
 
+  /**
+   *
+   * @param events$ incoming events
+   * @param next (event: E) => Observable<void>
+   * @returns transformed events Observable
+   */
   protected transformEvents(events$: Observable<E>, next: (event: E) => Observable<void>) {
     return events$.pipe(concatMap(next));
   }
 
+  /**
+   * @description subscribes to event stream to initialize a bloc and process events
+   * @returns Subscription
+   */
   private _subscribeToEvents(): Subscription {
     return this._events$
       .pipe(
@@ -80,6 +107,11 @@ export abstract class Bloc<E extends BlocEvent, State> extends Cubit<State> {
       .subscribe();
   }
 
+  /**
+   * @param event BlocEvent
+   * @returns Observable
+   * @description retrieves event handler from eventsMap if it exists and returns an empty observable
+   */
   private _mapEventToState(event: E): Observable<void> {
     const handler = this._eventsMap.get(event.blockEventName);
 
@@ -92,6 +124,11 @@ export abstract class Bloc<E extends BlocEvent, State> extends Cubit<State> {
     return result instanceof Promise ? from(result) : EMPTY;
   }
 
+  /**
+   *
+   * @param error
+   * @description error handler that returns an Observable that emits never as an exception has been thrown
+   */
   private _mapEventToStateError(error: Error): Observable<never> {
     this.onError(error);
     return EMPTY;
--- lib/cubit.ts
+++ lib/cubit.ts
@@ -8,18 +8,39 @@ export abstract class Cubit<T = any> {
     this._stateSubscription = this._subscribeToState();
   }
 
+  /**
+   * emits state pushed into a cubit
+   */
   readonly state$: Observable<T>;
+
   private readonly _stateSubject$: BehaviorSubject<T>;
   private readonly _stateSubscription: Subscription;
 
+  /**
+   * @returns the last emitted state in a cubit
+   */
   protected get state(): T {
     return this._state;
   }
 
+  /**
+   * @override
+   * @param current State
+   * @param next State
+   */
   protected onChange(current: T, next: T): void {}
 
+  /**
+   * @override
+   * @param error
+   */
   protected onError(error: Error): void {}
 
+  /**
+   *
+   * @param mapState (state: T) => K
+   * @returns new mapped selected state
+   */
   protected select<K>(mapState: (state: T) => K): Observable<K> {
     return this.state$.pipe(
       map((state) => mapState(state)),
@@ -27,6 +48,11 @@ export abstract class Cubit<T = any> {
     );
   }
 
+  /**
+   *
+   * @param newState new state to be emitted
+   * @returns void
+   */
   protected emit(newState: T): void {
     if (this._stateSubject$.closed) {
       return;
@@ -44,11 +70,20 @@ export abstract class Cubit<T = any> {
     this._stateSubscription.unsubscribe();
   }
 
+  /**
+   *
+   * @returns multicasted observable state
+   */
   private _buildStatePipeline(): Observable<T> {
     return this._stateSubject$.asObservable().pipe(shareReplay({ refCount: true, bufferSize: 1 }));
   }
 
-  protected listen(state: T) {
+  /**
+   *
+   * @param state current emitted state
+   * @returns void
+   */
+  protected listen(state: T): void {
     return;
   }
 
