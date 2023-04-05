220d98d3b7ae99efa633ae62b903254931795695
Jacob Tipp <jacobtipp@gmail.com>
2023-04-05 03:47:12 +0000

    docs(bloc-concurrency): 📚️ add typedoc comments

--- packages/bloc-concurrency/src/lib/bloc-concurrency.ts
+++ packages/bloc-concurrency/src/lib/bloc-concurrency.ts
@@ -1,17 +1,32 @@
 import { BlocEvent, EventTransformer } from '@jacobtipp/bloc';
 import { concatMap, mergeMap, switchMap } from 'rxjs';
 
+/**
+ * A function that returns a new EventTransformer that uses the `switchMap` operator to transform the input Observable.
+ * @typeparam Event The type of events passed to the Observable.
+ * @returns A new EventTransformer that applies the `switchMap` operator to the input Observable and maps each event to an Observable using the provided mapper function.
+ */
 export const restartable =
-  <E extends BlocEvent>(): EventTransformer<E> =>
+  <Event extends BlocEvent>(): EventTransformer<Event> =>
   (events$, mapper) =>
     events$.pipe(switchMap(mapper));
 
+/**
+ * A function that returns a new EventTransformer that uses the `concatMap` operator to transform the input Observable.
+ * @typeparam Event The type of events passed to the Observable.
+ * @returns A new EventTransformer that applies the `concatMap` operator to the input Observable and maps each event to an Observable using the provided mapper function.
+ */
 export const sequential =
-  <E extends BlocEvent>(): EventTransformer<E> =>
+  <Event extends BlocEvent>(): EventTransformer<Event> =>
   (events$, mapper) =>
     events$.pipe(concatMap(mapper));
 
+/**
+ * A function that returns a new EventTransformer that uses the `mergeMap` operator to transform the input Observable.
+ * @typeparam Event The type of events passed to the Observable.
+ * @returns A new EventTransformer that applies the `mergeMap` operator to the input Observable and maps each event to an Observable using the provided mapper function.
+ */
 export const concurrent =
-  <E extends BlocEvent>(): EventTransformer<E> =>
+  <Event extends BlocEvent>(): EventTransformer<Event> =>
   (events$, mapper) =>
     events$.pipe(mergeMap(mapper));
