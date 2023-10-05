a6f03503817765b5893eb700894672ed3bd8b710
Jacob Tipp <jacobtipp@gmail.com>
2023-10-04 18:06:44 -0700

    docs(license): 📚️ add license for @jacobtipp/state

--- packages/state/vite.config.ts
+++ packages/state/vite.config.ts
@@ -22,6 +22,10 @@ export default defineConfig({
           src: 'README.md',
           dest: '.',
         },
+        {
+          src: 'LICENSE',
+          dest: '.',
+        },
       ],
     }),
   ],
