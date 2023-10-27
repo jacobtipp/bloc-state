bf1ece398a6e5651f6d4acdf7fc87ce8e41d140f
Jacob Tipp <jacobtipp@gmail.com>
2023-10-26 18:45:35 -0700

    build: 📦️ change appsDir to apps instead of examples

--- apps/examples/hacker-news/vite.config.ts
+++ apps/examples/hacker-news/vite.config.ts
@@ -8,17 +8,24 @@ export default defineConfig({
     host: 'localhost',
   },
 
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      keep_classnames: true,
+    },
+  },
+
   plugins: [
     react(),
     viteTsConfigPaths({
-      root: '../../',
+      root: '../../../',
     }),
   ],
 
   test: {
     globals: true,
     cache: {
-      dir: '../../node_modules/.vitest',
+      dir: '../../../node_modules/.vitest',
     },
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
