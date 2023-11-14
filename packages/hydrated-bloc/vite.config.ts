3f48d79246ee478161c1b2b39726926a3a0a6c25
Jacob Tipp <jacobtipp@gmail.com>
2023-11-13 18:44:34 -0800

    build(deps): 📦️ add @jacobtipp/bloc as external dependency to @jacobtipp/hydrated-bloc

--- packages/hydrated-bloc/vite.config.ts
+++ packages/hydrated-bloc/vite.config.ts
@@ -45,7 +45,7 @@ export default defineConfig({
     },
     rollupOptions: {
       // External packages that should not be bundled into your library.
-      external: ['rxjs'],
+      external: ['@jacobtipp/bloc', 'rxjs'],
     },
   },
 });
