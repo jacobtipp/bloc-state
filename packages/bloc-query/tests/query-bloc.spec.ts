3a6d189c51ab9d42085a2a78857b31ac1bd02436
Jacob Tipp <jacobtipp@gmail.com>
2023-12-01 09:30:50 -0800

    feat(bloc-query): ✨ initial release for @jacobtipp/bloc-query package

---
+++ packages/bloc-query/tests/query-bloc.spec.ts
@@ -0,0 +1,317 @@
+//import { getRandomInt } from './helpers/random';
+import { take } from 'rxjs';
+import { GetQueryOptions, QueryBloc, QueryState } from '../src/lib';
+
+describe('QueryBloc', () => {
+  it('should not allow new subscriptions if the query is closed', (done) => {
+    const queryFn = () => {
+      return Promise.resolve(1);
+    };
+    const options: GetQueryOptions<number> = {
+      initialData: 0,
+      queryFn,
+      queryKey: '0',
+    };
+
+    const bloc = new QueryBloc<number>(
+      {
+        status: 'isInitial',
+        lastUpdatedAt: Date.now(),
+        isInitial: true,
+        isLoading: false,
+        isFetching: false,
+        isError: false,
+        isReady: true,
+        data: 0,
+      },
+      options
+    );
+
+    const states: QueryState<number>[] = [];
+
+    expect(bloc).toBeDefined();
+    expect(bloc.state.data).toBe(0);
+
+    bloc
+      .getQuery()
+      .pipe(take(2))
+      .subscribe({
+        next: (state) => states.push(state),
+        complete: () => {
+          const [a, b] = states;
+          expect(a.status).toBe('isFetching');
+          expect(b.status).toBe('isReady');
+          bloc.close();
+          try {
+            bloc.getQuery();
+          } catch (e) {
+            expect(e).toBeInstanceOf(Error);
+          }
+          done();
+        },
+      });
+  });
+
+  describe('Retry', () => {
+    let bloc: QueryBloc<number>;
+    let calls = 0;
+    let maxRetryAttempts: number;
+    let retryDuration: number;
+
+    afterEach(() => {
+      bloc.close();
+      calls = 0;
+      maxRetryAttempts = 0;
+      retryDuration = 0;
+    });
+
+    it('should handle error retries with default options', (done) => {
+      const queryFn = () => {
+        calls++;
+        throw new Error('Retry');
+      };
+      const options: GetQueryOptions<number> = {
+        initialData: 0,
+        queryFn,
+        queryKey: '0',
+        maxRetryAttempts,
+        retryDuration,
+      };
+      bloc = new QueryBloc<number>(
+        {
+          status: 'isInitial',
+          lastUpdatedAt: Date.now(),
+          isInitial: true,
+          isLoading: false,
+          isFetching: false,
+          isError: false,
+          isReady: true,
+          data: 0,
+        },
+        options
+      );
+
+      const states: QueryState<number>[] = [];
+
+      expect(bloc).toBeDefined();
+      expect(bloc.state.data).toBe(0);
+
+      bloc
+        .getQuery()
+        .pipe(take(2))
+        .subscribe({
+          next: (state) => states.push(state),
+          complete: () => {
+            const [a, b] = states;
+            expect(calls).toBe(2);
+            expect(a.status).toBe('isFetching');
+            expect(b.status).toBe('isError');
+            done();
+          },
+        });
+    });
+
+    it('should handle error retries with custom maxRetryAttempt', (done) => {
+      const queryFn = () => {
+        calls++;
+        throw new Error('Retry');
+      };
+
+      maxRetryAttempts = 2;
+      const options: GetQueryOptions<number> = {
+        initialData: 0,
+        queryFn,
+        queryKey: '0',
+        maxRetryAttempts,
+        retryDuration,
+      };
+      bloc = new QueryBloc<number>(
+        {
+          status: 'isInitial',
+          lastUpdatedAt: Date.now(),
+          isInitial: true,
+          isLoading: false,
+          isFetching: false,
+          isError: false,
+          isReady: true,
+          data: 0,
+        },
+        options
+      );
+
+      const states: QueryState<number>[] = [];
+
+      expect(bloc).toBeDefined();
+      expect(bloc.state.data).toBe(0);
+
+      bloc
+        .getQuery()
+        .pipe(take(2))
+        .subscribe({
+          next: (state) => states.push(state),
+          complete: () => {
+            const [a, b] = states;
+            expect(calls).toBe(maxRetryAttempts + 1);
+            expect(a.status).toBe('isFetching');
+            expect(b.status).toBe('isError');
+            done();
+          },
+        });
+    });
+
+    it('should disable retry if maxAttempts is set to 0', (done) => {
+      const queryFn = () => {
+        calls++;
+        throw new Error('Retry');
+      };
+
+      maxRetryAttempts = 0;
+      const options: GetQueryOptions<number> = {
+        initialData: 0,
+        queryFn,
+        queryKey: '0',
+        maxRetryAttempts,
+        retryDuration,
+      };
+      bloc = new QueryBloc<number>(
+        {
+          status: 'isInitial',
+          lastUpdatedAt: Date.now(),
+          isInitial: true,
+          isLoading: false,
+          isFetching: false,
+          isError: false,
+          isReady: true,
+          data: 0,
+        },
+        options
+      );
+
+      const states: QueryState<number>[] = [];
+
+      expect(bloc).toBeDefined();
+      expect(bloc.state.data).toBe(0);
+
+      bloc
+        .getQuery()
+        .pipe(take(2))
+        .subscribe({
+          next: (state) => states.push(state),
+          complete: () => {
+            const [a, b] = states;
+            expect(calls).toBe(maxRetryAttempts + 1);
+            expect(a.status).toBe('isFetching');
+            expect(b.status).toBe('isError');
+            done();
+          },
+        });
+    });
+
+    it('should handle error retries with custom retryDuration', (done) => {
+      const queryFn = () => {
+        calls++;
+        throw new Error('Retry');
+      };
+
+      maxRetryAttempts = 2;
+      retryDuration = 2000;
+
+      const options: GetQueryOptions<number> = {
+        initialData: 0,
+        queryFn,
+        queryKey: '0',
+        maxRetryAttempts,
+        retryDuration,
+      };
+      bloc = new QueryBloc<number>(
+        {
+          status: 'isInitial',
+          lastUpdatedAt: Date.now(),
+          isInitial: true,
+          isLoading: false,
+          isFetching: false,
+          isError: false,
+          isReady: true,
+          data: 0,
+        },
+        options
+      );
+
+      const states: QueryState<number>[] = [];
+
+      expect(bloc).toBeDefined();
+      expect(bloc.state.data).toBe(0);
+
+      const startTime: number = Date.now();
+      bloc
+        .getQuery()
+        .pipe(take(2))
+        .subscribe({
+          next: (state) => states.push(state),
+          complete: () => {
+            const duration = Date.now() - startTime;
+            const [a, b] = states;
+            expect(calls).toBe(maxRetryAttempts + 1);
+            expect(a.status).toBe('isFetching');
+            expect(b.status).toBe('isError');
+            expect(duration).toBeGreaterThan(retryDuration * maxRetryAttempts);
+            done();
+          },
+        });
+    });
+
+    it('should handle error retries with custom scalingDuration', (done) => {
+      const queryFn = () => {
+        calls++;
+        throw new Error('Retry');
+      };
+
+      maxRetryAttempts = 3;
+      const scalingDuration = 2000;
+
+      const options: GetQueryOptions<number> = {
+        initialData: 0,
+        queryFn,
+        queryKey: '0',
+        maxRetryAttempts,
+        retryDuration,
+        scalingDuration,
+      };
+      bloc = new QueryBloc<number>(
+        {
+          status: 'isInitial',
+          lastUpdatedAt: Date.now(),
+          isInitial: true,
+          isLoading: false,
+          isFetching: false,
+          isError: false,
+          isReady: true,
+          data: 0,
+        },
+        options
+      );
+
+      const states: QueryState<number>[] = [];
+
+      expect(bloc).toBeDefined();
+      expect(bloc.state.data).toBe(0);
+
+      const startTime: number = Date.now();
+      bloc
+        .getQuery()
+        .pipe(take(2))
+        .subscribe({
+          next: (state) => states.push(state),
+          complete: () => {
+            const duration = Date.now() - startTime;
+            const [a, b] = states;
+            expect(calls).toBe(maxRetryAttempts + 1);
+            expect(a.status).toBe('isFetching');
+            expect(b.status).toBe('isError');
+            expect(duration).toBeGreaterThan(12000);
+            done();
+          },
+        });
+    }, 13000);
+  });
+});
---
+++ packages/bloc-query/vite.config.ts
@@ -0,0 +1,51 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
+import dts from 'vite-plugin-dts';
+import { join } from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+export default defineConfig({
+  plugins: [
+    dts({
+      tsConfigFilePath: join(__dirname, 'tsconfig.lib.json'),
+      // Faster builds by skipping tests. Set this to false to enable type checking.
+      skipDiagnostics: true,
+    }),
+    react(),
+    viteTsConfigPaths({
+      root: '../../',
+    }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'README.md',
+          dest: '.',
+        },
+        {
+          src: 'LICENSE',
+          dest: '.',
+        },
+      ],
+    }),
+  ],
+
+  // Configuration for building your library.
+  // See: https://vitejs.dev/guide/build.html#library-mode
+  build: {
+    minify: 'esbuild',
+    lib: {
+      // Could also be a dictionary or array of multiple entry points.
+      entry: 'src/index.ts',
+      name: 'bloc-query',
+      fileName: 'index',
+      // Change this to the formats you want to support.
+      // Don't forgot to update your package.json as well.
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      // External packages that should not be bundled into your library.
+      external: ['@jacobtipp/bloc', 'rxjs'],
+    },
+  },
+});
