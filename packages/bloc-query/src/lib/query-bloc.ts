de85940805e666848559867f125379a40f601fc6
Jacob Tipp <jacobtipp@gmail.com>
2023-12-14 18:29:03 -0800

    refactor(bloc-query): ♻️ add private QueryBloc event handler methods

--- packages/bloc-query/src/lib/query-bloc.ts
+++ packages/bloc-query/src/lib/query-bloc.ts
@@ -1,4 +1,4 @@
-import { Bloc } from '@jacobtipp/bloc';
+import { Bloc, Emitter } from '@jacobtipp/bloc';
 import {
   Observable,
   OperatorFunction,
@@ -15,169 +15,69 @@ import {
   QuerySetQueryDataEvent,
   QueryErrorEvent,
 } from './query-event';
-import { FetchOptions, queryFetchTransformer } from './query-fetch-transformer';
+import {
+  FetchTransformerOptions,
+  queryFetchTransformer,
+} from './query-fetch-transformer';
 import { QueryState, Ready } from './query-state';
 
-/**
- * Represents a key used for querying data.
- * @typedef {string} QueryKey
- */
 export type QueryKey = string;
 
-/**
- * Represents optional configuration options for a query.
- * @typedef {Object} QueryOptions
- * @property {number} [staleTime] - The time (in milliseconds) data is considered stale.
- */
-type QueryOptions = {
+export type QueryOptions<Data> = {
+  initialData?: Data;
   staleTime?: number;
+  queryKey: QueryKey;
+  queryFn: (options: QueryFnOptions) => Promise<Data>;
 };
 
-/**
- * Represents options for a query function.
- * @typedef {Object} QueryFnOptions
- * @property {AbortSignal} signal - The signal used to abort the query.
- */
+export type QueryBlocOptions<Data> = {
+  name?: string;
+  staleTime?: number;
+} & FetchTransformerOptions &
+  QueryOptions<Data>;
+
 export type QueryFnOptions = {
   signal: AbortSignal;
 };
 
-/**
- * Represents options for creating a query and fetching data.
- * @template Data - The type of the data returned by the query.
- * @template Selected - The type of the selected data.
- * @typedef {Object} GetQueryOptions
- * @property {Data} [initialData] - The initial data for the query.
- * @property {string} [name] - The name of the query.
- * @property {(state: Ready<Data>) => Selected} [selector] - A function to select data from the query state.
- * @property {(previous: Selected, current: Selected) => boolean} [comparator] - A function to compare selected data.
- * @property {QueryKey} queryKey - The key used for querying data.
- * @property {(options: QueryFnOptions) => Promise<Data>} queryFn - The function to fetch data for the query.
- * @property {QueryOptions} [staleTime] - Additional query options.
- * @property {FetchOptions} - Additional fetch options.
- */
 export type GetQueryOptions<Data = unknown, Selected = QueryState<Data>> = {
-  initialData?: Data;
-  name?: string;
   selector?: (state: Ready<Data>) => Selected;
   comparator?: (previous: Selected, current: Selected) => boolean;
-  queryKey: QueryKey;
-  queryFn: (options: QueryFnOptions) => Promise<Data>;
-} & QueryOptions &
-  FetchOptions;
+} & QueryOptions<Data> &
+  FetchTransformerOptions;
 
 /**
  * Represents a Bloc for handling queries.
  * @template Data - The type of the data returned by the query.
- * @template Selected - The type of the selected data.
  * @extends {Bloc<QueryEvent, QueryState<Data>>}
  */
-export class QueryBloc<
-  Data = unknown,
-  Selected = QueryState<Data>
-> extends Bloc<QueryEvent, QueryState<Data>> {
+export class QueryBloc<Data = unknown> extends Bloc<
+  QueryEvent,
+  QueryState<Data>
+> {
   private staleTime: number;
   private handledInitialLoad = false;
 
   /**
    * Creates a new QueryBloc instance.
    * @param {QueryState<Data>} state - The initial state of the query.
-   * @param {GetQueryOptions<Data, Selected>} options - The options for creating the query.
+   * @param {QueryBlocOptions<Data>} options - The options for creating the query bloc.
    */
   constructor(
     state: QueryState<Data>,
-    options: GetQueryOptions<Data, Selected>
+    private options: QueryBlocOptions<Data>
   ) {
     super(state, options.name ?? options.queryKey);
     this.staleTime = options.staleTime ?? 0;
 
-    this.on(QuerySubscriptionEvent, (_event, _emit) => {
-      if (this.state.status === 'isLoading' && !this.handledInitialLoad) {
-        this.handledInitialLoad = true;
-        this.add(new QueryFetchEvent(new AbortController()));
-      }
-
-      if (this.state.isReady && this.isStale) {
-        this.add(new QueryRevalidateEvent());
-      }
-    });
-
-    this.on(QueryRevalidateEvent, (_event, emit) => {
-      emit({
-        status: 'isFetching',
-        lastUpdatedAt: this.state.lastUpdatedAt,
-        isInitial: false,
-        isLoading: false,
-        isFetching: true,
-        isReady: false,
-        isError: false,
-        data: this.state.data,
-      });
-
-      this.add(new QueryFetchEvent(new AbortController()));
-    });
-
-    this.on(QuerySetQueryDataEvent, (event, emit) => {
-      let newData: Data;
-      const setEvent = event as QuerySetQueryDataEvent<Data>;
-      const set = setEvent.set;
-      if (typeof set === 'function') {
-        if (this.state.data === undefined) {
-          throw new SetQueryDataException(
-            'SetQueryData: cannot be set with a callback function if previous data is undefined, invoke setQueryData with data directly ' +
-              'or provide initial data for the query'
-          );
-        }
-        newData = (set as (old: Data) => Data)(this.state.data);
-      } else {
-        newData = set;
-      }
-
-      emit({
-        status: 'isReady',
-        isInitial: false,
-        lastUpdatedAt: Date.now(),
-        isLoading: false,
-        isFetching: false,
-        isReady: true,
-        isError: false,
-        data: newData,
-      });
-    });
-
-    this.on(QueryErrorEvent, (event, emit) => {
-      emit({
-        status: 'isError',
-        lastUpdatedAt: this.state.lastUpdatedAt,
-        isInitial: false,
-        isLoading: false,
-        isFetching: false,
-        isReady: false,
-        isError: true,
-        data: this.state.data,
-        error: event.error,
-      });
-    });
-
+    this.on(QuerySubscriptionEvent, this.onQuerySubscription);
+    this.on(QueryRevalidateEvent, this.onQueryRevalidate);
+    this.on(QuerySetQueryDataEvent, this.onQuerySetQueryData);
+    this.on(QueryErrorEvent, this.onQueryError);
     this.on(
       QueryFetchEvent,
-      async (event, emit) => {
-        if (event.cancel) return;
-        const data = await options.queryFn({
-          signal: event.abortController.signal,
-        });
-        emit({
-          status: 'isReady',
-          lastUpdatedAt: Date.now(),
-          isInitial: false,
-          isLoading: false,
-          isFetching: false,
-          isReady: true,
-          isError: false,
-          data: data,
-        });
-      },
-      queryFetchTransformer<Data>(
+      this.onQueryFetch,
+      queryFetchTransformer(
         {
           maxRetryAttempts: options.maxRetryAttempts,
           retryDuration: options.retryDuration,
@@ -187,6 +87,106 @@ export class QueryBloc<
         this
       )
     );
+  }
+
+  private async onQueryFetch(
+    event: QueryFetchEvent,
+    emit: Emitter<QueryState<Data>>
+  ) {
+    if (event.cancel) return;
+    const data = await this.options.queryFn({
+      signal: event.abortController.signal,
+    });
+    emit({
+      status: 'isReady',
+      lastUpdatedAt: Date.now(),
+      isInitial: false,
+      isLoading: false,
+      isFetching: false,
+      isReady: true,
+      isError: false,
+      data: data,
+    });
+  }
+
+  private onQueryError(
+    event: QueryErrorEvent,
+    emit: Emitter<QueryState<Data>>
+  ) {
+    emit({
+      status: 'isError',
+      lastUpdatedAt: this.state.lastUpdatedAt,
+      isInitial: false,
+      isLoading: false,
+      isFetching: false,
+      isReady: false,
+      isError: true,
+      data: this.state.data,
+      error: event.error,
+    });
+  }
+
+  private onQuerySetQueryData(
+    event: QuerySetQueryDataEvent<unknown>,
+    emit: Emitter<QueryState<Data>>
+  ) {
+    let newData: Data;
+    const setEvent = event as QuerySetQueryDataEvent<Data>;
+    const set = setEvent.set;
+    if (typeof set === 'function') {
+      if (this.state.data === undefined) {
+        throw new SetQueryDataException(
+          'SetQueryData: cannot be set with a callback function if previous data is undefined, invoke setQueryData with data directly ' +
+            'or provide initial data for the query'
+        );
+      }
+      newData = (set as (old: Data) => Data)(this.state.data);
+    } else {
+      newData = set;
+    }
+
+    emit({
+      status: 'isReady',
+      isInitial: false,
+      lastUpdatedAt: Date.now(),
+      isLoading: false,
+      isFetching: false,
+      isReady: true,
+      isError: false,
+      data: newData,
+    });
+  }
+
+  private onQueryRevalidate(
+    _event: QueryRevalidateEvent,
+    emit: Emitter<QueryState<Data>>
+  ) {
+    emit({
+      status: 'isFetching',
+      lastUpdatedAt: this.state.lastUpdatedAt,
+      isInitial: false,
+      isLoading: false,
+      isFetching: true,
+      isReady: false,
+      isError: false,
+      data: this.state.data,
+    });
+
+    this.add(new QueryFetchEvent(new AbortController()));
+  }
+
+  private onQuerySubscription(
+    _event: QuerySubscriptionEvent,
+    _emit: Emitter<QueryState<Data>>
+  ) {
+    if (this.state.status === 'isLoading' && !this.handledInitialLoad) {
+      this.handledInitialLoad = true;
+      this.add(new QueryFetchEvent(new AbortController()));
+    }
+
+    if (this.state.isReady && this.isStale) {
+      this.add(new QueryRevalidateEvent());
+    }
   }
 
   /**
