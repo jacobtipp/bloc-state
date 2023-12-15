f2c8f941c67a54679582e4ae2bb963e2dbe8005b
Jacob Tipp <jacobtipp@gmail.com>
2023-12-14 16:09:06 -0800

    style(bloc-query): 🎨 add JSDoc comments

--- packages/bloc-query/src/lib/query-bloc.ts
+++ packages/bloc-query/src/lib/query-bloc.ts
@@ -18,16 +18,44 @@ import {
 import { FetchOptions, queryFetchTransformer } from './query-fetch-transformer';
 import { QueryState, Ready } from './query-state';
 
+/**
+ * Represents a key used for querying data.
+ * @typedef {string} QueryKey
+ */
 export type QueryKey = string;
 
+/**
+ * Represents optional configuration options for a query.
+ * @typedef {Object} QueryOptions
+ * @property {number} [staleTime] - The time (in milliseconds) data is considered stale.
+ */
 type QueryOptions = {
   staleTime?: number;
 };
 
+/**
+ * Represents options for a query function.
+ * @typedef {Object} QueryFnOptions
+ * @property {AbortSignal} signal - The signal used to abort the query.
+ */
 export type QueryFnOptions = {
   signal: AbortSignal;
 };
 
+/**
+ * Represents options for creating a query and fetching data.
+ * @template Data - The type of the data returned by the query.
+ * @template Selected - The type of the selected data.
+ * @typedef {Object} GetQueryOptions
+ * @property {Data} [initialData] - The initial data for the query.
+ * @property {string} [name] - The name of the query.
+ * @property {(state: Ready<Data>) => Selected} [selector] - A function to select data from the query state.
+ * @property {(previous: Selected, current: Selected) => boolean} [comparator] - A function to compare selected data.
+ * @property {QueryKey} queryKey - The key used for querying data.
+ * @property {(options: QueryFnOptions) => Promise<Data>} queryFn - The function to fetch data for the query.
+ * @property {QueryOptions} [staleTime] - Additional query options.
+ * @property {FetchOptions} - Additional fetch options.
+ */
 export type GetQueryOptions<Data = unknown, Selected = QueryState<Data>> = {
   initialData?: Data;
   name?: string;
@@ -38,6 +66,12 @@ export type GetQueryOptions<Data = unknown, Selected = QueryState<Data>> = {
 } & QueryOptions &
   FetchOptions;
 
+/**
+ * Represents a Bloc for handling queries.
+ * @template Data - The type of the data returned by the query.
+ * @template Selected - The type of the selected data.
+ * @extends {Bloc<QueryEvent, QueryState<Data>>}
+ */
 export class QueryBloc<
   Data = unknown,
   Selected = QueryState<Data>
@@ -45,6 +79,11 @@ export class QueryBloc<
   private staleTime: number;
   private handledInitialLoad = false;
 
+  /**
+   * Creates a new QueryBloc instance.
+   * @param {QueryState<Data>} state - The initial state of the query.
+   * @param {GetQueryOptions<Data, Selected>} options - The options for creating the query.
+   */
   constructor(
     state: QueryState<Data>,
     options: GetQueryOptions<Data, Selected>
@@ -150,11 +189,23 @@ export class QueryBloc<
     );
   }
 
+  /**
+   * Checks if the query data is stale based on the last update time and stale time.
+   * @type {boolean}
+   */
   get isStale() {
     const now = Date.now();
     return this.state.lastUpdatedAt + this.staleTime <= now;
   }
 
+  /**
+   * Gets an observable for the query state.
+   * @template Selected - The type of the selected data.
+   * @param {(state: Ready<Data>) => Selected} [selector] - A function to select data from the query state.
+   * @param {(previous: Selected, current: Selected) => boolean} [comparer] - A function to compare selected data.
+   * @returns {Observable<Selected>} - An observable for the selected data.
+   * @throws {QueryClosedException} - If the query is closed.
+   */
   getQuery = <Selected = QueryState<Data>>(
     selector?: (state: Ready<Data>) => Selected,
     comparer?: (previous: Selected, current: Selected) => boolean
@@ -175,27 +226,53 @@ export class QueryBloc<
     );
   };
 
+  /**
+   * Sets new data for the query.
+   * @param {((old: Data) => Data) | Data} set - The new data or a function to update the old data.
+   */
   setQueryData = (set: ((old: Data) => Data) | Data) => {
     this.add(new QuerySetQueryDataEvent(set));
   };
 
+  /**
+   * Cancels the query, aborting the ongoing fetch operation.
+   */
   cancelQuery = () => {
     this.add(new QueryFetchEvent(new AbortController(), true));
   };
 
+  /**
+   * Revalidates the query, triggering a new fetch operation.
+   */
   revalidateQuery = () => {
     this.add(new QueryRevalidateEvent());
   };
 }
 
+/**
+ * Represents an exception thrown when attempting to set query data in an invalid manner.
+ * @extends {Error}
+ */
 export class SetQueryDataException extends Error {
+  /**
+   * Creates a new SetQueryDataException instance.
+   * @param {string} message - The error message.
+   */
   constructor(message: string) {
     super(message);
     Object.setPrototypeOf(this, SetQueryDataException.prototype);
   }
 }
 
+/**
+ * Represents an exception thrown when attempting to interact with a closed query.
+ * @extends {Error}
+ */
 export class QueryClosedException extends Error {
+  /**
+   * Creates a new QueryClosedException instance.
+   * @param {string} message - The error message.
+   */
   constructor(message: string) {
     super(message);
     Object.setPrototypeOf(this, QueryClosedException.prototype);
