c033e9833b6f4b157094157f28a4cd3a8377acf4
Jacob Tipp <jacobtipp@gmail.com>
2023-12-17 18:48:40 -0800

    build(apps): 📦️ define process.env.NODE_ENV for vite apps in development

--- apps/bloc-todos/vite.config.ts
+++ apps/bloc-todos/vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(process.env['NODE_ENV']),
+  },
   cacheDir: '../../node_modules/.vite/bloc-todos',
 
   server: {
