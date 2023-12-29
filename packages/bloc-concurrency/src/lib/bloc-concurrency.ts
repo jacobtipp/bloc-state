ede8830fa5ddd43abb945ea0e5628c3212960dee
Jacob Tipp <jacobtipp@gmail.com>
2023-12-28 16:54:21 -0800

    refactor(bloc-concurrency): ♻️ update docs and return type for event transformers

--- packages/bloc-concurrency/src/lib/bloc-concurrency.ts
+++ packages/bloc-concurrency/src/lib/bloc-concurrency.ts
@@ -1,32 +1,64 @@
-import { EventTransformer } from '@jacobtipp/bloc';
-import { concatMap, mergeMap, switchMap } from 'rxjs';
+import { EventMapper, EventTransformer } from '@jacobtipp/bloc';
+import { concatMap, Observable, mergeMap, switchMap } from 'rxjs';
 
 /**
- * A function that returns a new EventTransformer that uses the `switchMap` operator to transform the input Observable.
- * @typeparam Event The type of events passed to the Observable.
- * @returns A new EventTransformer that applies the `switchMap` operator to the input Observable and maps each event to an Observable using the provided mapper function.
+ * A function that returns a new EventTransformer that uses the `switchMap` operator
+ * to transform the input Observable.
+ *
+ * @template Event - The type of events passed to the Observable.
+ * @returns {EventTransformer<Event>} A new EventTransformer that applies the `switchMap` operator
+ * to the input Observable and maps each event to an Observable using the provided mapper function.
  */
 export const restartable =
   <Event>(): EventTransformer<Event> =>
-  (events$, mapper) =>
+  /**
+   * Transforms the input observable stream using the `switchMap` operator,
+   * applying the provided mapper function to each event.
+   *
+   * @param {Observable<Event>} events$ - The input observable stream.
+   * @param {EventMapper<Event>} mapper - A function to map events.
+   * @returns {Observable<Event>} The transformed observable stream.
+   */
+  (events$: Observable<Event>, mapper: EventMapper<Event>): Observable<Event> =>
     events$.pipe(switchMap(mapper));
 
 /**
- * A function that returns a new EventTransformer that uses the `concatMap` operator to transform the input Observable.
- * @typeparam Event The type of events passed to the Observable.
- * @returns A new EventTransformer that applies the `concatMap` operator to the input Observable and maps each event to an Observable using the provided mapper function.
+ * A function that returns a new EventTransformer that uses the `concatMap` operator
+ * to transform the input Observable.
+ *
+ * @template Event - The type of events passed to the Observable.
+ * @returns {EventTransformer<Event>} A new EventTransformer that applies the `concatMap` operator
+ * to the input Observable and maps each event to an Observable using the provided mapper function.
  */
 export const sequential =
   <Event>(): EventTransformer<Event> =>
-  (events$, mapper) =>
+  /**
+   * Transforms the input observable stream using the `concatMap` operator,
+   * applying the provided mapper function to each event.
+   *
+   * @param {Observable<Event>} events$ - The input observable stream.
+   * @param {EventMapper<Event>} mapper - A function to map events.
+   * @returns {Observable<Event>} The transformed observable stream.
+   */
+  (events$: Observable<Event>, mapper: EventMapper<Event>): Observable<Event> =>
     events$.pipe(concatMap(mapper));
 
 /**
- * A function that returns a new EventTransformer that uses the `mergeMap` operator to transform the input Observable.
- * @typeparam Event The type of events passed to the Observable.
- * @returns A new EventTransformer that applies the `mergeMap` operator to the input Observable and maps each event to an Observable using the provided mapper function.
+ * Creates a function that concurrently transforms an observable stream
+ * by applying the provided mapper to each event without waiting for completion.
+ *
+ * @template Event - The type of events in the observable stream.
+ * @returns {EventTransformer<Event>} A function that concurrently transforms the observable stream.
  */
 export const concurrent =
   <Event>(): EventTransformer<Event> =>
-  (events$, mapper) =>
+  /**
+   * Concurrently transforms the input observable stream by applying the
+   * provided mapper to each event without waiting for completion.
+   *
+   * @param {Observable<Event>} events$ - The input observable stream.
+   * @param {EventMapper<Event>} mapper - A function to map events.
+   * @returns {Observable<Event>} The transformed observable stream.
+   */
+  (events$: Observable<Event>, mapper: EventMapper<Event>): Observable<Event> =>
     events$.pipe(mergeMap(mapper));
