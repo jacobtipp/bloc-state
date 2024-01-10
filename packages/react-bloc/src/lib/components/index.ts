e4377cf6922965b487caa6f9b788bd55a8ca687c
Jacob Tipp <jacobtipp@gmail.com>
2024-01-09 18:37:24 -0800

    feat(react-bloc): ✨ add BlocBuilder component

--- packages/react-bloc/src/lib/components/index.ts
+++ packages/react-bloc/src/lib/components/index.ts
@@ -2,3 +2,4 @@ export * from './listener';
 export * from './bloc-provider';
 export * from './error-boundary';
 export * from './repository-provider';
+export * from './bloc-builder';
--- packages/react-bloc/src/lib/hooks/defaults.ts
+++ packages/react-bloc/src/lib/hooks/defaults.ts
@@ -2,3 +2,4 @@ export const defaultSuspendWhen = () => false;
 export const defaultListenWhen = () => true;
 export const defaultErrorWhen = () => false;
 export const defaultSelector = (state: unknown) => state;
+export const defaultBuildWhen = () => true;
