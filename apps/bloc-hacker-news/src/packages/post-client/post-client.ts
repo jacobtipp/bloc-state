6be9150e4175a5fb50977ebdaf1e40486f033dfb
Jacob Tipp <jacobtipp@gmail.com>
2024-01-16 20:50:43 -0800

    style(apps): 🎨 format bloc-hacker-news example

--- apps/bloc-hacker-news/src/packages/post-client/post-client.ts
+++ apps/bloc-hacker-news/src/packages/post-client/post-client.ts
@@ -1,7 +1,7 @@
 import { Post } from './model/post';
 
 export abstract class PostClient {
-  protected _!: void
-  abstract cancelPost(id: number): void
+  protected _!: void;
+  abstract cancelPost(id: number): void;
   abstract getPost(id: number): Promise<Post>;
 }
