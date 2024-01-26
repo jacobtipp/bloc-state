326121b1279132b9d36e42fde921fe9640ca3d21
Jacob Tipp <jacobtipp@gmail.com>
2024-01-26 12:55:30 -0800

    feat(react-bloc): ✨ add getServerSnapshot in useBlocValue and useBlocSelector

---
+++ packages/react-bloc/tests/lib/use-bloc-selector.spec.tsx
@@ -0,0 +1,22 @@
+import { render } from '@testing-library/react';
+import { CounterExample } from './counter';
+import { renderToString } from 'react-dom/server';
+import { ContextMapProvider } from '../../src';
+
+describe('useBlocSelector', () => {
+  it('should render on the server', async () => {
+    expect.assertions(1);
+    const ui = (
+      <ContextMapProvider>
+        <CounterExample count={5} />
+      </ContextMapProvider>
+    );
+    const container = document.createElement('div');
+    container.innerHTML = renderToString(ui);
+
+    const { findByTestId } = render(ui, { hydrate: true, container });
+
+    const count = await findByTestId('count');
+    expect(count.textContent).toBe('5');
+  }, 5000);
+});
