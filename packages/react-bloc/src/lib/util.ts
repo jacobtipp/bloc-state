fd22e6523cb76df6f161ce58e6fae32206fd293b
Jacob Tipp <jacobtipp@gmail.com>
2024-01-25 19:09:19 -0800

    refactor(react-bloc): ♻️ add experimental React.use hook for handling suspense

--- packages/react-bloc/src/lib/util.ts
+++ packages/react-bloc/src/lib/util.ts
@@ -1,7 +1,40 @@
-import { useEffect, useLayoutEffect } from 'react';
+import React, { useEffect, useLayoutEffect } from 'react';
 
 export const isServer = typeof window === 'undefined';
 
 export const useIsomorphicLayoutEffect = (() =>
   /* istanbul ignore next */
   typeof window === 'undefined' ? useEffect : useLayoutEffect)();
+
+// this is from jotai useAtomValue https://github.com/pmndrs/jotai/blob/main/src/react/useAtomValue.ts
+/* istanbul ignore next */
+export const use =
+  React.use ||
+  (<T>(
+    promise: PromiseLike<T> & {
+      status?: 'pending' | 'fulfilled' | 'rejected';
+      value?: T;
+      reason?: unknown;
+    }
+  ): T => {
+    if (promise.status === 'pending') {
+      throw promise;
+    } else if (promise.status === 'fulfilled') {
+      return promise.value as T;
+    } else if (promise.status === 'rejected') {
+      throw promise.reason;
+    } else {
+      promise.status = 'pending';
+      promise.then(
+        (v) => {
+          promise.status = 'fulfilled';
+          promise.value = v;
+        },
+        (e) => {
+          promise.status = 'rejected';
+          promise.reason = e;
+        }
+      );
+      throw promise;
+    }
+  });
