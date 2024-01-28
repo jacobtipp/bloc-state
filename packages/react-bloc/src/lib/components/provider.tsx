d514c1eb778a3de9c726338ecefd757f8a2c69c2
Jacob Tipp <jacobtipp@gmail.com>
2024-01-27 22:54:12 -0800

    feat(react-bloc): ✨ add disposeTimer prop for Provider

--- packages/react-bloc/src/lib/components/provider.tsx
+++ packages/react-bloc/src/lib/components/provider.tsx
@@ -21,31 +21,56 @@ export interface ProviderProps<Class extends AnyClassType> {
   create: () => InstanceType<Class>;
   onMount?: (instance: InstanceType<Class>) => void;
   onUnmount?: (instance: InstanceType<Class>) => void;
+  disposeTime?: number;
   children: ReactNode;
   dependencies?: any[];
 }
+
+const mounted = new WeakSet<any>();
 
 export const Provider = <Class extends AnyClassType>({
   children,
   classDef,
   dependencies = [],
+  disposeTime = 5 * 1000,
   create,
   onMount,
   onUnmount,
 }: ProviderProps<Class>) => {
-  const instance = useDisposable(() => {
+  const createInstance = () => {
     const instance = create();
+    setTimeout(() => {
+      /* istanbul ignore else */
+      if (!mounted.has(instance)) {
+        onUnmount?.(instance);
+      }
+    }, disposeTime);
+    return instance;
+  };
+
+  const instance = useDisposable(() => {
+    const instance = createInstance();
     return [
       instance,
       () => {
         onUnmount?.(instance);
       },
     ];
-  }, dependencies);
+  }, dependencies) as InstanceType<Class>;
 
   useEffect(() => {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    onMount?.(instance!);
+    const created = instance;
+
+    onMount?.(created);
+
+    /* istanbul ignore else */
+    if (!mounted.has(created)) {
+      mounted.add(created);
+    }
+
+    return () => {
+      mounted.delete(created);
+    };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [instance]);
 
