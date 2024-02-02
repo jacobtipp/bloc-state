c8f3de97bc40df6c793dfb3574d2add43b939fba
Jacob Tipp <jacobtipp@gmail.com>
2024-02-02 00:35:43 -0800

    chore(apps): 🔧 remove bloc-hacker-news app

--- apps/bloc-hacker-news/src/features/posts/models/post-view-model.ts
+++
@@ -1,9 +0,0 @@
-import { Post } from '../../../packages/post-client/model/post';
-
-export type PostViewModel = {
-  postId: {
-    previousId?: number;
-    currentId: number;
-  };
-  details: Post;
-};
